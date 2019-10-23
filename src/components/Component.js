097f4ce83da5b4dd0bcd677a12b5dff7f3a34960
jujuniort06 <julio_thomazelli-ju1@estudante.sc.senai.br>
2019-10-23 20:36:00 -0300

    Setup

---
+++ src/components/Component.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function C_SelectField() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import C_SelectField from './components/Component';
+
+ReactDOM.render(<C_SelectField />, document.getElementById('root'));
+
